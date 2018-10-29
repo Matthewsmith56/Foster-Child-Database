c1be3d42ebb0a48b5870e6c01bfa420dc6812adf
Matthew <Matthew@DESKTOP-FBBVVQH>
2018-10-28 20:41:25 -0400

    Created basic database with table

---
+++ FosterData/FosterTable.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[FosterTable]
+(
+	[Family ID]					INT		IDENTITY (1, 1) NOT NULL,
+	[Foster Family Name]		NVARCHAR	(50)		NULL,
+	[Type of Home]				NVARCHAR	(50)		NULL,
+	[Number of Licensed Beds]	INT						NULL,
+	[Number of Occupied Beds]	INT						NULL,
+	[Number of Available Beds]	INT						NULL,
+	[Minimum Age]				INT						NULL,
+	[Maximum Age]				INT						NULL,
+	[Gender Preferance]			NVARCHAR	(20)		NULL,
+	[Managing Agency]			NVARCHAR	(20)		NULL,
+	[Point of Contact]			NVARCHAR	(20)		NULL,
+	[Comments]					NVARCHAR	(100)		NULL,
+	PRIMARY KEY CLUSTERED ([Family ID] ASC)
+
+)
---
+++ FosterSite/Controllers/FosterTablesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FosterSite.Models;
+
+namespace FosterSite.Controllers
+{
+    public class FosterTablesController : Controller
+    {
+        private FosterDataEntities db = new FosterDataEntities();
+
+        // GET: FosterTables
+        public ActionResult Index()
+        {
+            return View(db.FosterTables.ToList());
+        }
+
+        // GET: FosterTables/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FosterTable fosterTable = db.FosterTables.Find(id);
+            if (fosterTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fosterTable);
+        }
+
+        // GET: FosterTables/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FosterTables/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Family_ID,Foster_Family_Name,Type_of_Home,Number_of_Licensed_Beds,Number_of_Occupied_Beds,Number_of_Available_Beds,Minimum_Age,Maximum_Age,Gender_Preferance,Managing_Agency,Point_of_Contact,Comments")] FosterTable fosterTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.FosterTables.Add(fosterTable);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(fosterTable);
+        }
+
+        // GET: FosterTables/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FosterTable fosterTable = db.FosterTables.Find(id);
+            if (fosterTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fosterTable);
+        }
+
+        // POST: FosterTables/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Family_ID,Foster_Family_Name,Type_of_Home,Number_of_Licensed_Beds,Number_of_Occupied_Beds,Number_of_Available_Beds,Minimum_Age,Maximum_Age,Gender_Preferance,Managing_Agency,Point_of_Contact,Comments")] FosterTable fosterTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(fosterTable).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(fosterTable);
+        }
+
+        // GET: FosterTables/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FosterTable fosterTable = db.FosterTables.Find(id);
+            if (fosterTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fosterTable);
+        }
+
+        // POST: FosterTables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            FosterTable fosterTable = db.FosterTables.Find(id);
+            db.FosterTables.Remove(fosterTable);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
