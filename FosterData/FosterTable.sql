d93e9d89106524789aa9213393a304d10ae94a1e
Matthew <Matthew@DESKTOP-FBBVVQH>
2018-10-31 13:27:12 -0400

    Added simple form validation

--- FosterData/FosterTable.sql
+++ FosterData/FosterTable.sql
@@ -3,18 +3,14 @@
 	[Family ID]					INT		IDENTITY (1, 1) NOT NULL,
 	[Foster Family]				NVARCHAR	(50)		NULL,
 	[Type of Home]				NVARCHAR	(50)		NULL,
-	[Licensed Beds]				INT						NOT NULL, --CHECK ([Licensed Beds]  >= 0),
-	[Occupied Beds]				INT						NOT NULL, --CHECK ([Occupied Beds]  <= [Licensed Beds]),
-	[Available Beds]			INT						NOT NULL, --CHECK ([Available Beds] <= [Licensed Beds]),
-	[Min Age]					INT						NOT NULL, --CHECK ([Min Age]		  >= 0),
-	[Max Age]					INT						NOT NULL, --CHECK ([Max Age]		  <= 18),
+	[Licensed Beds]				INT						NULL, --CHECK ([Licensed Beds]  >= 0),
+	[Occupied Beds]				INT						NULL, --CHECK ([Occupied Beds]  <= [Licensed Beds]),
+	[Available Beds]			INT						NULL, --CHECK ([Available Beds] <= [Licensed Beds]),
+	[Min Age]					INT						NULL, --CHECK ([Min Age]		  >= 0),
+	[Max Age]					INT						NULL, --CHECK ([Max Age]		  <= 18),
 	[Preferred Gender]			NVARCHAR	(20)		NULL,
 	[Agency]					NVARCHAR	(20)		NULL,
-	[POC]						NVARCHAR	(20)		NULL,
+	[POC]						NVARCHAR	(50)		NULL,
 	[Comments]					NVARCHAR	(100)		NULL,
 	PRIMARY KEY CLUSTERED ([Family ID] ASC)
 )
-
-
-
-
---
+++ FosterSite/Models/FosterTableConstraints.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace FosterSite.Models
+{
+    [MetadataType(typeof(FosterTableMetaData))]
+    public partial class FosterTable
+    {
+    }
+
+    public class FosterTableMetaData
+    {
+        public int Family_ID { get; set; }
+
+        [Required(ErrorMessage = "Family Name is required")]
+        public string Foster_Family { get; set; }
+
+        [Required(ErrorMessage = "Home type is required")]
+        public string Type_of_Home { get; set; }
+
+        [Range(0, 10), Required(ErrorMessage = "Number of licensed beds is required")]
+        public Nullable<int> Licensed_Beds { get; set; }
+
+        [Range (0, 10), Required(ErrorMessage = "Number of occupied beds is required")]
+        public Nullable<int> Occupied_Beds { get; set; }
+
+        [Range(0, 10), Required(ErrorMessage = "Number of available beds is required")]
+        public Nullable<int> Available_Beds { get; set; }
+
+        public Nullable<int> Min_Age { get; set; }
+        public Nullable<int> Max_Age { get; set; }
+        public string Preferred_Gender { get; set; }
+
+        [Required(ErrorMessage = "Agency is required")]
+        public string Agency { get; set; }
+
+        [Required(ErrorMessage = "A point of contact is required")]
+        public string POC { get; set; }
+
+        public string Comments { get; set; }
+    }
+}
